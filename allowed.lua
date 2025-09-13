5930527740e2264c474c26ae49fb743ed86b9bd8
ArchIsDead <ccryptohigh@gmail.com>
2025-09-13 11:52:49 +0700

    Update allowed.lua

--- allowed.lua
+++ allowed.lua
@@ -1,3 +1,8 @@
-Shalderal
-kaiton_280
-maktashuw7
+local allowedUsers = {
+    3532024703,  -- Shalderal (main user)
+    32851559,    -- Another user
+    123456789,   -- Test user
+    987654321    -- Backup user
+}
+
+return allowedUsers
